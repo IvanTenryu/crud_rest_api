0e2d0683f5232b383adad6858887236b3d090542
Ivan Israel Valencia Barreda <52473941+IvanTenryu@users.noreply.github.com>
2021-12-23 10:31:31 -0600

    Requirements file created

--- src/api/models/authors.py
+++ src/api/models/authors.py
@@ -5,8 +5,7 @@ from api.models.books import BookSchema
 class Author(db.Model):
   __tablename__ = 'authors'
 
-  id = db.Column(db.Integer, primary_key=True,
-  autoincrement=True)
+  id = db.Column(db.Integer, primary_key=True, autoincrement=True)
   first_name = db.Column(db.String(20))
   last_name = db.Column(db.String(20))
   created = db.Column(db.DateTime, server_default=db.func.now())
