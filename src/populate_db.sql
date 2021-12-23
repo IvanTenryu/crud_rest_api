5a077f0f78410af3699de8100262318b24c2fb19
Ivan Israel Valencia Barreda <52473941+IvanTenryu@users.noreply.github.com>
2021-12-23 10:22:17 -0600

    Table population script created

---
+++ src/populate_db.sql
@@ -0,0 +1,19 @@
+# Llenar tabla authors con datos de prueba
+INSERT INTO authors (`first_name`,`last_name`)
+VALUES ('JK','Rowling');
+
+INSERT INTO authors (`first_name`,`last_name`)
+VALUES ('Gerorge','RR Martin');
+
+INSERT INTO authors (`first_name`,`last_name`)
+VALUES ('Ivan','Ilich');
+
+# LLenar tabla books con datos de puebra
+INSERT INTO books (`title`,`year`)
+VALUES ('Harry Potter','2000');
+
+INSERT INTO books (`title`,`year`)
+VALUES ('Game of Thrones','1992');
+
+INSERT INTO books (`title`,`year`)
+VALUES ('Shadow Work','1981');
